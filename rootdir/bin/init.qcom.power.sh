15004763624637eb70efa4bab39cf2c087db7c6c
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2016-12-24 00:22:25 +0200

    power: Reduce usage of Big cores

--- rootdir/bin/init.qcom.power.sh
+++ rootdir/bin/init.qcom.power.sh
@@ -108,8 +108,8 @@ write /proc/sys/kernel/sched_migration_fixup 1
 write /proc/sys/kernel/sched_small_task 30
 write /proc/sys/kernel/sched_mostly_idle_load 20
 write /proc/sys/kernel/sched_mostly_idle_nr_run 3
-write /proc/sys/kernel/sched_downmigrate 50
-write /proc/sys/kernel/sched_upmigrate 70
+write /proc/sys/kernel/sched_downmigrate 85
+write /proc/sys/kernel/sched_upmigrate 99
 write /proc/sys/kernel/sched_init_task_load 50
 write /proc/sys/kernel/sched_freq_inc_notify 400000
 write /proc/sys/kernel/sched_freq_dec_notify 400000
