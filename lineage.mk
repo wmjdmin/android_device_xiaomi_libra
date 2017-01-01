ae8b4479ad95c6d080e625fde0293f48260f8e6e
Serhij Kyryljan <kirilyan.sergei@gmail.com>
2017-01-01 12:55:44 +0200

    libra: Move to LineageOS

--- lineage.mk
+++ lineage.mk
@@ -3,14 +3,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/libra/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_libra
+PRODUCT_NAME := lineage_libra
 PRODUCT_DEVICE := libra
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi-4c
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,4 @@
 # limitations under the License.
 #
 
-add_lunch_combo cm_libra-userdebug
+add_lunch_combo lineage_libra-userdebug
