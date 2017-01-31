ffe901d16be33c84e953337001c372796e081938
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-01-31 08:12:31 +0200

    Update and clean up
    GPS from Mi Max.

--- camera/Android.mk
+++ camera/Android.mk
@@ -16,12 +16,19 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := camera/sensor.cpp camera/SensorManager.cpp
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
 
-LOCAL_C_INCLUDES := camera
-LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid
-LOCAL_MODULE := libshim_camera
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libutils libgui
+
+LOCAL_C_INCLUDES += \
+    system/media/camera/include
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+LOCAL_32_BIT_ONLY := $(BOARD_QTI_CAMERA_32BIT_ONLY)
 
 include $(BUILD_SHARED_LIBRARY)
